c825782900df94c5f88f7134a70c7195db774ec3
Jason Toy <jasontoy@gmail.com>
2017-02-11 14:43:58 -0500

    saving works

---
+++ README.md
@@ -0,0 +1,14 @@
+# p6js
+My experiments with working on a next generation creative coding environment
+
+# experiments
+* realtime documentation browser
+* live coding
+* tweak mode
+
+# TODOS
+* open projects
+* save (fo rwhen autosave is not being used)
+* clojurescript integration
+
+copywrite Jason Toy
--- main.js
+++ main.js
@@ -7,6 +7,11 @@ const app = electron.app
 const BrowserWindow = electron.BrowserWindow
 
 const ipc = require('electron').ipcMain
+const dialog = electron.dialog;
+const fs = require('fs'); 
+
+var globalFilePath
+var testing = "wtf"
 
 ipc.on('asynchronous_load_scratch', function (event, arg) {
   load_scratch()
@@ -41,10 +46,77 @@ function createWindow () {
 app.on('ready', createWindow)
 app.on('ready', () => {
   // Register a 'CommandOrControl+Y' shortcut listener.
-  globalShortcut.register('CommandOrControl+R', () => {
+  //globalShortcut.register('CommandOrControl+R', () => {
     //put code to run code here
-    load_scratch()
-  })
+    //load_scratch()
+  //})
+})
+
+const Menu = electron.Menu
+let template = [{
+  label: 'Edit',
+  submenu: [{
+    label: 'Run',
+    accelerator: 'CmdOrCtrl+R',
+    click: function (item, focusedWindow) {
+      load_scratch()
+    }
+  }, {
+    label: 'Open',
+    accelerator: 'CmdOrCtrl+O',
+    role: 'open'
+  }, {
+    label: 'Save',
+    accelerator: 'CmdOrCtrl+S',
+    click: function(item,focusedWindow){
+      if (globalFilePath == undefined) {
+      //  saveFileDialog()
+      } else {
+       // mainWindow.webContents.send('getSave');
+      }
+    }
+  }, {
+    label: 'Save As',
+    accelerator: 'CmdOrCtrl+Shift+S',
+    click: function(item,focusedWindow){
+      saveFileDialog()
+    }
+  }]
+}, {
+  label: 'Window',
+  role: 'window',
+  submenu: [{
+    label: 'Minimize',
+    accelerator: 'CmdOrCtrl+M',
+    role: 'minimize'
+  }, {
+    label: 'Close',
+    accelerator: 'CmdOrCtrl+W',
+    role: 'close'
+  }, {
+    type: 'separator'
+  }, {
+    label: 'Reopen Window',
+    accelerator: 'CmdOrCtrl+Shift+T',
+    enabled: false,
+    key: 'reopenMenuItem',
+    click: function () {
+      app.emit('activate')
+    }
+  }]
+}, {
+  label: 'Help',
+  role: 'help',
+  submenu: [{
+    label: 'Learn More',
+    click: function () {
+      electron.shell.openExternal('https://github.com/jtoy/p6js')
+    }
+  }]
+}]
+app.on('ready', function () {
+  const menu = Menu.buildFromTemplate(template)
+  Menu.setApplicationMenu(menu)
 })
 
 // Quit when all windows are closed.
@@ -65,10 +137,85 @@ app.on('activate', function () {
 })
 
 function load_scratch(){
-  var fs = require('fs'); 
   const {width, height} = electron.screen.getPrimaryDisplay().workAreaSize
   newWindow = new BrowserWindow({width: width, height: height})
   var path = `file://${__dirname}/app/template/index.html`
   console.log(path)
   newWindow.loadURL(path)
 }
+
+
+function saveFileDialog(callback) {
+  dialog.showSaveDialog({
+  }, function(filePath) {
+    // Get the content from the renderer
+    mainWindow.webContents.send('getSaveAs');
+    console.log("SADSDSDDSD")
+    ipc.on('fileSaveAs', function(event, data) {
+    console.log("IIIISADSDSDDSD")
+      writeFile(filePath, data) 
+      //writeFile(filePath, data, function(callback) {
+        //BECUASE THIS DOESNT TAKE MUTIPLE FUNCTIONS?
+        console.log("HERER")
+        error('success', "<strong>Success!</strong> File Saved");
+        globalFilePath = filePath;
+        console.log("SDASDSDSDS")
+        var array = globalFilePath.split("/");
+        filename = array[array.length - 1];
+        //callback();
+      });
+    //}
+    //);
+  });
+}
+
+ipc.on('fileSave', function(event, data) {
+  writeFile(globalFilePath, data);
+});
+
+function error(type, message) {
+  var data = {
+    type: type,
+    message: message
+  };
+  mainWindow.webContents.send('error', data);
+}
+
+function writeFile(filePath, contents) {
+  if (filePath != undefined) {
+    fs.writeFile(filePath, contents, (err) => {
+      if (err) {
+        error('danger', "<strong>Uh-Oh!</strong> There was an error saving the file.");
+      } else {
+        error("success", "<strong>Success!</strong> The file has been saved.");
+        mainWindow.setTitle("p6js - " + filePath);
+      }
+    });
+  }
+}
+
+function save_scratch_or_global_file(content){
+  if(globalFilePath){
+    var filepath = globalFilePath
+  }else{
+    var filepath = `${__dirname}/app/template/sketch.js`
+  }
+  console.log("wtf: "+filepath)
+
+
+  fs.writeFile(filepath, content, function (err) {
+    if(err){
+      console.log(err);
+      return;
+    }
+  });
+
+}
+ipc.on('async_save_scratch_or_global_file', function(event, data) {
+  save_scratch_or_global_file(data);
+});
+
+
+
+
+
